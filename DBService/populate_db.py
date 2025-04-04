842e71779a0d0eaf7d98a9cd6dce33bfe04427b3
leonardomf12 <LeonardoEng12@gmail.com>
2025-04-04 22:29:59 +0100

    feat - psql container with correct schema + dataset integration to dataservice

--- DBService/populate_db.py
+++ DBService/populate_db.py
@@ -4,12 +4,15 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from pathlib import Path
 
+from db_tables import Image
+
 
 DB_USER = "postgres"
 DB_PASSWD = 12345
-DB_HOSTNAME = "psql_database"
+DB_HOSTNAME = "localhost"
 DB_NAME = "mydatabase"
 DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWD}@{DB_HOSTNAME}/{DB_NAME}"
+
 engine = create_engine(DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(bind=engine)
 
@@ -23,35 +26,35 @@ def get_dataset_files():
     file_paths = []
     for f in dataset_path.rglob("*"):
         if f.is_file():
-            file_paths.append("/".join(f.split("/")[-2:]))
+            file_paths.append("/".join(str(f).split("/")[-2:]))
 
     return file_paths
 
 
 # Insert into PostgreSQL
-def insert_file_paths(file_paths, chunk_size=100):
+def insert_file_paths(image_paths, chunk_size=100):
     session = SessionLocal() # opens session
 
     try:
-        for i in range(0, len(file_paths), chunk_size):
-            batch = file_paths[i:i + chunk_size]
-            # Prepare data for bulk insert
-            session.bulk_save_objects([DatasetFile(file_path=fp) for fp in batch])
-            session.commit()  # Commit each batch
-            print(f"Inserted {len(batch)} records.")
+        for i in range(0, len(image_paths), chunk_size):
+            chunk = image_paths[i:i + chunk_size]
+            values = [Image(image_path=path) for path in chunk]
+
+            session.add_all(values)
+            session.flush()
+            session.commit()
+
+            session.expunge_all()
+
+        print("Dataset inserted successfully")
+
     except Exception as e:
-        session.rollback()
+        session.rollback() # Reverts all changes in this transaction
+        print(f"\nError uploading image_paths: \n{e}")
+    finally:
+        session.close()
 
 
 if __name__ == "__main__":
     files = get_dataset_files()
-
-    IPython.embed()
-
-    # Run the script
-    #file_paths = get_all_files(DIRECTORY_PATH)
-    #insert_file_paths(file_paths)
-
-    # Cleanup
-    #cur.close()
-    #conn.close()
+    insert_file_paths(files)
